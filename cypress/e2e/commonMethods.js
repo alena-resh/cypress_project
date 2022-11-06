8201a1eaf4794bb66c5b4c0e0b9835afb0156084
alena-resh <reshetnikovaalenna@gmail.com>
2022-11-06 14:39:46 +0300

    new test - visible img

---
+++ cypress/e2e/commonMethods.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+
+
+ describe('Common methods', () => {
+it('Verify that user can navigate to Elements page', () => {
+    cy.visit('https://demoqa.com/')
+    cy.get('header a img').should('be.visible')
+})
+
+
+ })
