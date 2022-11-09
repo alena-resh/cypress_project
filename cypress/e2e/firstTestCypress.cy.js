27aa40782cef81c2e4f51d8206f37e2677f428a9
alena-resh <reshetnikovaalenna@gmail.com>
2022-11-09 13:15:30 +0300

    little changes

--- cypress/e2e/firstTestCypress.cy.js
+++ cypress/e2e/firstTestCypress.cy.js
@@ -1,5 +1,5 @@
 describe('URL type navigation', function () {
-    it('Clicking "type" navigates to a new URL', function () {
+    it('clicking "type" navigates to a new URL', function () {
         cy.visit('https://example.cypress.io')
         cy.contains('type').click()
         cy.url().should('include', '/commands/actions')
@@ -7,15 +7,15 @@ describe('URL type navigation', function () {
 })
 
 describe('URL siblings navigation', function(){
-    it('Clicking "siblings navigates to URL', function(){
+    it('clicking "siblings navigates to URL', function(){
         cy.visit('https://example.cypress.io')
         cy.contains('siblings').click()
         cy.url().should('include', '/commands/traversal')
     })
 })
 
-describe('Commands URL navigation', () => {
-    it('Checking menu button for commands', () => {
+describe('commands URL navigation', () => {
+    it('checking menu button for commands', () => {
         cy.visit('https://example.cypress.io/')
         cy.get('a.dropdown-toggle').click()
     })
