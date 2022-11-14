17062e4b08cd66d26bc570681fb9223ecfdea4ba
alena-resh <reshetnikovaalenna@gmail.com>
2022-11-14 17:35:37 +0300

    some changes in tests

--- cypress/e2e/openWeatherMap.cy.js
+++ cypress/e2e/openWeatherMap.cy.js
@@ -41,7 +41,7 @@ describe('8-day forecast', () => {
     })
 
     it('verify redirection to the "Terms and conditions of sale" page', () => {
-        cy.get('[href*="conditions_of_sale"]').invoke('removeAttr', 'target').click( {force: true} )
+        cy.get('[href*="conditions_of_sale"]').invoke('removeAttr', 'target').click({ force: true })
         cy.url().should('eq', 'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf')
     })
 })
