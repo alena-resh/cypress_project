53aef06e18271d12e0350e8c3d312b11d90b6fe4
alena-resh <reshetnikovaalenna@gmail.com>
2022-11-14 17:33:26 +0300

    new openWeather tests added

--- cypress/e2e/openWeatherMap.cy.js
+++ cypress/e2e/openWeatherMap.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+
+
 describe('8-day forecast', () => {
     beforeEach(function () {
         cy.visit('https://openweathermap.org/')
@@ -7,23 +9,23 @@ describe('8-day forecast', () => {
             this.data = data
         })
     })
-    it('navigate to 8-day forecast', () => {
+    xit('navigate to 8-day forecast', () => {
         cy.get('div.daily-container h3').should('have.text', '8-day forecast')
     })
 
-    it('8-day forecast has list', () => {
+    xit('8-day forecast has list', () => {
         let dateNow = new Date().toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: '2-digit' })
         cy.visit('https://openweathermap.org/')
         cy.get("ul[class='day-list'] li:nth-child(1) span:nth-child(1)")
             .should('have.text', dateNow)
     })
 
-    it('remember me radio button', () => {
+    xit('remember me radio button', () => {
         cy.get('li.user-li').contains('Sign in').click({ force: true })
         cy.get('#user_remember_me').check().should('be.checked')
     })
 
-    it('sign in - enter email and password', () => {
+    xit('verify "Sign in" - enter email and password', () => {
         cy.get('li.user-li').contains('Sign in').click({ force: true })
         cy.get('#user_email')
             .should('have.attr', 'placeholder', 'Enter email')
@@ -32,5 +34,14 @@ describe('8-day forecast', () => {
             .should('have.attr', 'placeholder', 'Password')
             .type('12341234')
     })
-})
 
+    xit('verify "Partners" menu link', () => {
+        cy.get('#desktop-menu a[href="/examples"]').click({ force: true });
+        cy.url().should('eq', 'https://openweathermap.org/examples')
+    })
+
+    it('verify redirection to the "Terms and conditions of sale" page', () => {
+        cy.get('[href*="conditions_of_sale"]').invoke('removeAttr', 'target').click( {force: true} )
+        cy.url().should('eq', 'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf')
+    })
+})
