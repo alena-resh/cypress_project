f4dfea37325b8c828d789c6d2390da8ef2fe0515
alena-resh <reshetnikovaalenna@gmail.com>
2022-11-20 23:31:02 +0300

    new tests

--- cypress/e2e/openWeatherMap.cy.js
+++ cypress/e2e/openWeatherMap.cy.js
@@ -29,19 +29,31 @@ describe('8-day forecast', () => {
         cy.get('li.user-li').contains('Sign in').click({ force: true })
         cy.get('#user_email')
             .should('have.attr', 'placeholder', 'Enter email')
-            .type('alenareshh@gmail.com')
+            .type('reshh@gmail.com')
         cy.get('#user_password.form-control')
             .should('have.attr', 'placeholder', 'Password')
             .type('12341234')
+        cy.get('#user_remember_me').check().should('be.checked')
+        cy.contains('Submit').click()
+        cy.get('.panel-body').should('have.text', 'Signed in successfully.')
     })
 
     xit('verify "Partners" menu link', () => {
-        cy.get('#desktop-menu a[href="/examples"]').click({ force: true });
+        cy.get('#desktop-menu a[href="/examples"]').click({ force: true })
         cy.url().should('eq', 'https://openweathermap.org/examples')
     })
 
     it('verify redirection to the "Terms and conditions of sale" page', () => {
         cy.get('[href*="conditions_of_sale"]').invoke('removeAttr', 'target').click({ force: true })
         cy.url().should('eq', 'https://openweather.co.uk/storage/app/media/Terms/Openweather_terms_and_conditions_of_sale.pdf')
+    })
+
+    it.only('verify clicking the first "Try the Dashboard" button not authorized - user is redirected to "Sign in" page', () => {
+        cy.get('#user-dropdown').should('not.exist')
+        cy.get('#desktop-menu [href="/weather-dashboard"]').click({force: true})
+        cy.get('.breadcrumb-title').should('be.visible').and('include.text','Weather dashboard')
+        cy.get('.btn_like.btn-orange.owm-block-mainpage__btn').eq(0).contains('Try the Dashboard').invoke('removeAttr','target').click()
+        cy.url().should('include','/users/sign_in')
+        cy.get('.sign-form').should('exist')
     })
 })
